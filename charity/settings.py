1ea98895b07547f3a48e0928a7d34ef04adc1d47
moa <moa.davarzani@gmail.com>
2021-04-05 19:44:10 +0430

    update settings.py file

--- charity/settings.py
+++ charity/settings.py
@@ -108,6 +108,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
